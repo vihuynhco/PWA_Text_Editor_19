7c94a49966bec748a7d3960a494ae3ff73fa82be
vihuynhco <vihuynhco@gmail.com>
2023-06-29 19:36:16 -0600

    restructured file to remove develop folder

---
+++ client/src/js/database.js
@@ -0,0 +1,40 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id:1, content:content});
+  const result =  await request;
+  await tx.done;
+  console.log('jate database updated', result);                                                     
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result =  await request;
+  await tx.done;
+  console.log('jate database retrieved', result);
+  await tx.done;
+  console.log('jate database retrieved');
+  return content;
+}
+
+initdb();
